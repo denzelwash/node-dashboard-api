52878fbd385509a872800d6c9e61a81dff3ab224
Denis Obolevich <denzelwash33@mail.ru>
2022-12-17 11:16:09 +0300

    рефакторинг папок

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,7 @@ app.use('/users', userRouter);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 	console.log(err.message);
+	
 	res.status(401).send(err.message);
 });
 
