bd0d9236f9f77505c3a8f70dbe7368a8cf1f1691
Denis Obolevich <denzelwash33@mail.ru>
2022-12-21 21:02:53 +0300

    exeption filter

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { App } from './app.js'
+import { ExeptionFilter } from './errors/exeption.filter.js'
 import { LoggerService } from './logger/logger.service'
 import { UserController } from './users/users.controller.js'
 
 const bootstrap = async () => {
   const logger = new LoggerService()
-  const app = new App(logger, new UserController(logger))
+  const app = new App(logger, new UserController(logger), new ExeptionFilter(logger))
   await app.init()
 }
 
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
 import { Request, NextFunction, Response } from "express";
 import { BaseController } from "../common/base.controller";
+import { HTTPError } from "../errors/http.error.class";
 import { LoggerService } from "../logger/logger.service";
 
 export class UserController extends BaseController {
@@ -20,7 +21,7 @@ export class UserController extends BaseController {
   }
 
   login(req: Request, res: Response, next: NextFunction) {
-    this.ok(res, 'login')
+    next(new HTTPError(401, 'Ошибка авторизации', 'login'))
   }
 
   register(req: Request, res: Response, next: NextFunction) {
